230491b19631d49b2adbee4f304f5cd8842781dd
patrykok <165296918+patrykok@users.noreply.github.com>
2026-02-24 18:05:31 +0100

    dodano zmiane prędkości na podłogach

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform groundCheck;
+    public LayerMask groundMask;
 [SerializeField] float speed = 12f;
 Vector3 velocity;
 CharacterController characterController;
@@ -17,6 +19,27 @@ PlayerMove();
 }
 void PlayerMove()
 {
+    RaycastHit hit;
+    if (Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 0.4f, groundMask))
+    {
+        string terrainType;
+        terrainType = hit.collider.gameObject.tag;
+
+        switch (terrainType)
+        {
+            case "High":
+            speed = 20;
+            break;
+            case "Low":
+            speed = 3;
+            break;
+            default:
+            speed = 12;
+            break;
+        }
+    }
+
+
 float x = Input.GetAxis("Horizontal");
 float z = Input.GetAxis("Vertical");
 Vector3 move = transform.right * x + transform.forward * z;
